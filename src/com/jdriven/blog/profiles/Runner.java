cebdee685c05ed09e51d6fecc98057e3557832b4
Michel Meeuwissen <mmeeuw@gmail.com>
2015-03-08 18:40:55 +0100

    Some extra comments added to the code

--- src/com/jdriven/blog/profiles/Runner.java
+++ src/com/jdriven/blog/profiles/Runner.java
@@ -1,15 +1,31 @@
 package com.jdriven.blog.profiles;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class Runner {
 
 	public static void main(String[] args) {
+        //Create new context to show the XML Spring profile setup
 		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
-		//Setting active profile
+		//Setting 'ldap' as active profile
 		ctx.getEnvironment().setActiveProfiles("ldap");
+        //Load the app-context.xml from the root of the classpath
 		ctx.load("classpath:app-context.xml");
-		ctx.refresh();
-		ctx.close();
+		//We need to refresh the application because we added a resource
+        ctx.refresh();
+		//Closing the application context to release and destroy all resources and cached beans
+        ctx.close();
+
+        //Creating a new context to show the annotation Spring profile setup
+        AnnotationConfigApplicationContext actx = new AnnotationConfigApplicationContext();
+        //Setting 'saml' as active profile
+        actx.getEnvironment().setActiveProfiles("saml");
+        //Scan base package for annotations
+        actx.scan("com.jdriven.blog");
+        //We need to refresh the application because we added a scan
+        actx.refresh();
+        //Closing the application context to release and destroy all resources and cached beans
+        actx.close();
 	}
 }
